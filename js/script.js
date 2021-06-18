18a231c6e8a2d342cc910d23456e896d6f214c3a
Flavio <flavio.origgi@outlook.it>
2021-06-18 14:49:01 +0200

    fermo

--- js/script.js
+++ js/script.js
@@ -4,19 +4,18 @@
 
 const items = [1, 2, 40, 30, 8, 50, 25, 29, 80];
 
-let a = parseInt(prompt('inserire un numero da 0 a '  + ((items.length)-2)));
+let a = parseInt(prompt('inserire un numero da 1 a '  + ((items.length)-2)));
 let b = parseInt(prompt('inserire un numero da ' + a + ' a ' + items.length + ' e che sia diverso da prima'));
 
 let range;
+let newItems = [];
 
-getRange(items, a, b);
-
+document.getElementById('testo').innerHTML += newItems;
 function getRange(items, a, b){
-    range = items.filter((element) =>{
-        const newItems = [];
+    range = items.filter((element) =>{       
         if(element >= a && element <= b){
-            console.log(range);
-        }
+            newItems.push(element);
+        }        
     });    
 }
 
